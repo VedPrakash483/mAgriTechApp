df9e721b150d18e53c7153bb04cb7339ebd12ab1
Asad <asadalpha77@gmail.com>
2024-10-31 16:24:48 +0530

    added student page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:e_agritech_app/farmer/dashboard.dart';
 import 'package:e_agritech_app/firebase_options.dart';
+import 'package:e_agritech_app/student/home_page_student.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'screens/welcome_screen.dart';
 import 'services/firebase_auth_service.dart'; // Make sure this is correct
-import 'providers/auth_provider.dart'; // Import your auth provider if you have one
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,7 +23,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => FirebaseAuthService()), // Ensure this is correct
+        ChangeNotifierProvider(
+            create: (_) => FirebaseAuthService()), // Ensure this is correct
         // Add other providers as needed
       ],
       child: MaterialApp(
@@ -31,8 +33,9 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.green,
           visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: GoogleFonts.poppins().fontFamily,
         ),
-        home: const WelcomeScreen(),
+        home: const HomePageStudent(),
       ),
     );
   }
