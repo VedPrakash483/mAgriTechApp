b620ef980f9ce3585ec47dcd819f3e1bd117d907
Asad <asadalpha77@gmail.com>
2024-11-17 19:49:32 +0530

    asad

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
 import 'package:e_agritech_app/auth/auth_checker.dart';
-import 'package:e_agritech_app/farmer/dashboard.dart';
-import 'package:e_agritech_app/farmer/problem_detail_screen.dart';
 import 'package:e_agritech_app/firebase_options.dart';
-import 'package:e_agritech_app/student/home_page_student.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-import 'screens/welcome_screen.dart';
-import 'services/firebase_auth_service.dart'; // Make sure this is correct
+import 'services/firebase_auth_service.dart'; 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   DefaultFirebaseOptions.currentPlatform;
   await Firebase.initializeApp();
-
-  runApp(const MyApp());
+runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
